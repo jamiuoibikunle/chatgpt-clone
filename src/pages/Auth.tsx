e679edbaa02b5cfb7e46a388c09df84407f29168
Jamiu Ibikunle <ibidotun@gmail.com>
2023-03-09 18:59:39 +0100

    Added auth

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,3 +1,21 @@
+import { Button, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+
 export default () => {
-  return <>Auth</>;
+  return (
+    <VStack h="100vh" w="100%" justifyContent="center" spacing={3}>
+      <Image src="/icon.svg" />
+      <Heading fontSize="1rem" fontWeight="500">
+        Welcome to ChatGPT
+      </Heading>
+      <Text>Log in with your OpenAI account to continue</Text>
+      <HStack>
+        <Button fontWeight="500" colorScheme="green">
+          Log in
+        </Button>
+        <Button fontWeight="500" colorScheme="green">
+          Sign up
+        </Button>
+      </HStack>
+    </VStack>
+  );
 };
